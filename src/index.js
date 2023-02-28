78b46e6ae8c97e9c6aff3076de8351ca36b8df7b
Esteban Velasquez <eavc.30@gmail.com>
2023-02-28 12:45:03 -0500

    feat: add intro, outro and commit message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('commits-cli')
+import { text, outro, intro } from '@clack/prompts'
+
+intro('Wizard for the creation of commits by @evelasquez')
+
+const commitMessage = await text({
+  message: 'Commit message',
+  placeholder: 'feat: add new feature'
+})
+
+console.log(commitMessage)
+
+outro('Commit created successfully. Thanks for using the wizard!')
