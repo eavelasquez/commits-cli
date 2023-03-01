5e3bf6e9e67634f6063fc0e1166aec3ef82a8cc3
Esteban Velasquez <eavc.30@gmail.com>
2023-02-28 19:53:57 -0500

    feat: update commit-types file

--- src/commit-types.js
+++ src/commit-types.js
@@ -1,32 +1,32 @@
 export const COMMIT_TYPES = {
   feat: {
     emoji: '✨',
-    description: 'feat: add new feature',
+    description: 'add new feature',
     release: true
   },
   fix: {
     emoji: '🐛',
-    description: 'fix: submit a bug fix',
+    description: 'submit a bug fix',
+    release: true
+  },
+  refac: {
+    emoji: '🛠',
+    description: 'submit a code refactoring',
     release: true
   },
   docs: {
     emoji: '📚',
-    description: 'docs: add or update documentation',
+    description: 'add or update documentation',
     release: false
-  },
-  refac: {
-    emoji: '🛠',
-    description: 'refactor: submit a code refactoring',
-    release: true
   },
   test: {
     emoji: '🧪',
-    description: 'test: add or update tests',
+    description: 'add or update tests',
     release: false
   },
   build: {
     emoji: '👷',
-    description: 'build: submit a build system change',
+    description: 'submit a build system change',
     release: false
   }
 }
