c6476421ccc984c2b9a48b00f9b8ba3696541c0c
Esteban Velasquez <eavc.30@gmail.com>
2023-02-28 19:57:17 -0500

    feat: add exit program function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
+import { outro } from '@clack/prompts'
+
 export function cleanStdout (stdout) {
   return stdout.trim().split('\n').filter(Boolean)
+}
+
+export function exitProgram ({ code = 0, message = 'A commit has not been created' } = {}) {
+  outro(message)
+  process.exit(code)
 }
